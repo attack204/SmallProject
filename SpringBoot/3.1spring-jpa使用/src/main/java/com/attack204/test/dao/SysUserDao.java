e0b5b1618816e04da5e349b075fad66bbe4d05d1
attack204 <757394026@qq.com>
2021-03-25 18:30:48 +0800

    update

--- SpringBoot/3.1spring-jpa使用/src/main/java/com/attack204/test/dao/SysUserDao.java
+++ SpringBoot/3.1spring-jpa使用/src/main/java/com/attack204/test/dao/SysUserDao.java
@@ -2,12 +2,12 @@ package com.attack204.test.dao;
 
 import com.attack204.test.po.SysUserPo;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 /**
  * @author attack204
@@ -18,4 +18,14 @@ import java.util.Optional;
  */
 @Repository
 public interface SysUserDao extends PagingAndSortingRepository<SysUserPo, Long> {
+    List<SysUserPo> findAllByCorp(Long corp);
+    SysUserPo findByCorp(Long corp);
+    SysUserPo findByCorpAndUsername(Long corp, String username);
+//    Page<SysUserPo> findByPasswordContainingOrUsernameContaining(String password, String username, PageRequest pageRequest);
+    Page<SysUserPo> findSysUsersByPasswordContainingOrUsernameContaining(String password, String username, Pageable pageable);
+    //注意这里一定要写Pageable！！不要写PageRequest
+//    Page<SysUserPo> findSysUserPosByStatus(Integer status, Pageable pageable);
+//    Page<SysUserPo> findSysUserPoByStatus(Integer status, Pageable pageable);
+    Page<SysUserPo> findByStatus(Integer status, Pageable pageable);
+    //这种三种写法没啥区别
 }
--- SpringBoot/3.1spring-jpa使用/src/test/java/com/attack204/test/TestApplicationTests.java
+++ SpringBoot/3.1spring-jpa使用/src/test/java/com/attack204/test/TestApplicationTests.java
@@ -6,6 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.awt.print.Pageable;
+import java.sql.SQLOutput;
+import java.util.List;
 
 @SpringBootTest
 class TestApplicationTests {
@@ -14,10 +21,41 @@ class TestApplicationTests {
     SysUserDao sysUserDao;
 
     @Test
-    void contextLoads() {
-        SysUserPo target = sysUserDao.findById(17L).get();
-        target.setUsername("fuck5");
-        sysUserDao.save(target);
+    void PageTest() {
+        //下面的代码可以执行对SysUserPo对象对应表的分页查询，并将结果按Sort指定的顺序返回
+        //不过比较遗憾的是,jpa内部指支持对所有数据的分页
+        int PageNumber = 0;
+        int PageSize = 3;
+        Sort sort = Sort.by(Sort.Order.asc("id"));
+        PageRequest pageRequest = PageRequest.of(PageNumber, PageSize, sort);
+        Page<SysUserPo> result = sysUserDao.findAll(pageRequest);
+        System.out.println("数据总条数" + result.getTotalElements());
+        System.out.println("总页数" + result.getTotalPages());
+        List<SysUserPo> list = result.getContent();
+        for(SysUserPo sysUserPo: list) {
+            System.out.println(sysUserPo);
+        }
     }
-
+    @Test
+    void SortTest() {
+        Sort sort = Sort.by(Sort.Order.asc("corp"));
+        List<SysUserPo> list = (List<SysUserPo>) sysUserDao.findAll(sort);
+        for(SysUserPo sysUserPo: list) {
+            System.out.println(sysUserPo);
+        }
+    }
+    @Test
+    void PageDIYTest() {
+        int PageNumber = 0;
+        int PageSize = 3;
+//        Sort sort = Sort.by(Sort.Order.asc("id"));
+        PageRequest pageRequest = PageRequest.of(PageNumber, PageSize);
+        Page<SysUserPo> result = sysUserDao.findSysUsersByPasswordContainingOrUsernameContaining("at", "at",   pageRequest);
+//        System.out.println("数据总条数" + result.getTotalElements());
+//        System.out.println("总页数" + result.getTotalPages());
+//        List<SysUserPo> list = result.getContent();
+//        for(SysUserPo sysUserPo: list) {
+//            System.out.println(sysUserPo);
+//        }
+    }
 }
