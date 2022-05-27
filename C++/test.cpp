9c9b09b1b5d0a44332879e597a3b19dbe8a36624
liugaoji <757394026@qq.com>
2022-05-27 17:07:20 +0800

    update

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,24 +1,40 @@
 #include<iostream>
 using namespace std;
 
-class X {
-
-};
-class A: virtual X {
+class Point3d {
+    xxx
 public:
-    int sum() {
-        return 1;
+    Point3d normalize() const {
+        register float msg = magnitude();
+        Point3d normal;
+        normal._x = _x / msg;
+        normal._y = _y / msg;
+        normla._z = _z / msg;
+        return normal;
     }
 };
-class B: virtual X {
 
-};
-class D: A, B {
 
-};
+void normalize_point3d7v(const Point3d * const this, Point3d &result) {
+    register float msg = magnitude();
+    result.Point3d::Point3d(); //constructor
+    result._x = this->_x / msg;
+    result._y = this->_y / msg;
+    result._z = this->_z / msg;
+    return
+}
+
 
 int main() {
-    A a;
-    cout << a.sum() << '\n';
+    Point3d obj;
+    Point3d *ptr = &obj;
+    obj.normalize();
+    ptr->normalize();
+
+
+    Point3d result
+    normalize_point3d7v(&obj, result)
+    normalize_point3d7v(&ptr, result)
+
 }
 
