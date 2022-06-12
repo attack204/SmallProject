5abbad43d5b4b62d2821fe79876f5b6d2034663b
liugaoji <757394026@qq.com>
2022-06-12 15:38:13 +0800

    update

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,40 +1,12 @@
+#include<cstdio>
 #include<iostream>
 using namespace std;
+class A                     { public: void eat(){ cout<<"A";} }; 
+class B: virtual public A   { public: void eat(){ cout<<"B";} }; 
+class C: virtual public A   { public: void eat(){ cout<<"C";} }; 
+class D: public         B,C {  }; 
 
-class Point3d {
-    xxx
-public:
-    Point3d normalize() const {
-        register float msg = magnitude();
-        Point3d normal;
-        normal._x = _x / msg;
-        normal._y = _y / msg;
-        normla._z = _z / msg;
-        return normal;
-    }
-};
-
-
-void normalize_point3d7v(const Point3d * const this, Point3d &result) {
-    register float msg = magnitude();
-    result.Point3d::Point3d(); //constructor
-    result._x = this->_x / msg;
-    result._y = this->_y / msg;
-    result._z = this->_z / msg;
-    return
-}
-
-
-int main() {
-    Point3d obj;
-    Point3d *ptr = &obj;
-    obj.normalize();
-    ptr->normalize();
-
-
-    Point3d result
-    normalize_point3d7v(&obj, result)
-    normalize_point3d7v(&ptr, result)
-
-}
-
+int main(){ 
+    D *a = new D(); 
+    a->eat(); 
+} 
